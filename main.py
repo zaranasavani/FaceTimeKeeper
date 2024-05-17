6bee034175ced15182d9221a79a418ff2c501d39
zarana savani <129183177+zaranasavani@users.noreply.github.com>
2024-05-17 18:21:00 +0530

    Update main.py

--- main.py
+++ main.py
@@ -11,8 +11,8 @@ video_capture = cv2.VideoCapture(0)
 person1_image = fr.load_image_file("face/person1.jpg")       # store images in "face"
 person1_encoding = fr.face_encodings(person1_image)[0]
 
-hinal_image = fr.load_image_file("face/hinal.jpeg")
-hinal_encoding = fr.face_encodings(hinal_image)[0]
+person2_image = fr.load_image_file("face/person2.jpeg")
+person2_encoding = fr.face_encodings(person2_image)[0]
 
 known_face_encodings = [zarana_encoding,hinal_encoding]
 known_face_names = ["zarana","hinal"]
